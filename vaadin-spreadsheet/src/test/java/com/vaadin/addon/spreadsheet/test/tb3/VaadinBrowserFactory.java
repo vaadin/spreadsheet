7c1d2c11dc8de83d83c09d1d241cf4d945c26dec
rogozinds <rogozinds@gmail.com>
2017-02-15 09:55:32 +0200

    Run tests on Firefox 45

--- vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/tb3/VaadinBrowserFactory.java
+++ vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/tb3/VaadinBrowserFactory.java
@@ -1,6 +1,8 @@
 package com.vaadin.addon.spreadsheet.test.tb3;
 
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -9,8 +11,10 @@ import com.vaadin.testbench.parallel.DefaultBrowserFactory;
 
 public class VaadinBrowserFactory extends DefaultBrowserFactory {
 
-    @Override
-    public DesiredCapabilities create(Browser browser) {
+    private static final String FIREFOX_VERSION = "45";
+    private static final String CHROME_VERSION = "40";
+
+    @Override public DesiredCapabilities create(Browser browser) {
         switch (browser) {
         case IE8:
             return createIE(browser, "8");
@@ -23,27 +27,41 @@ public class VaadinBrowserFactory extends DefaultBrowserFactory {
         case PHANTOMJS:
             DesiredCapabilities phantom2 = create(browser, "2", Platform.LINUX);
             // Hack for the test cluster
-            phantom2.setCapability("phantomjs.binary.path",
-                    "/usr/bin/phantomjs2");
+            phantom2
+                .setCapability("phantomjs.binary.path", "/usr/bin/phantomjs2");
             return phantom2;
         case CHROME:
-            return create(browser, "40", Platform.VISTA);
+            return create(browser, CHROME_VERSION, Platform.VISTA);
         case FIREFOX:
         default:
-            return create(browser, "24", Platform.XP);
+            return createFirefox();
         }
     }
 
     private DesiredCapabilities createIE(Browser browser, String version) {
         DesiredCapabilities capabilities = create(browser, version,
-                Platform.WINDOWS);
-        capabilities.setCapability(
-                InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION, true);
+            Platform.WINDOWS);
+        capabilities
+            .setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION,
+                true);
         return capabilities;
     }
 
-    @Override
-    public DesiredCapabilities create(Browser browser, String version) {
+    private DesiredCapabilities createFirefox() {
+        DesiredCapabilities desiredCapabilities = create(Browser.FIREFOX,
+            FIREFOX_VERSION, Platform.WINDOWS);
+        desiredCapabilities.setCapability("marionette", "false");
+        // Configuring an empty Firefox profile fixes tests
+        // where a data point is clicked.
+
+        FirefoxProfile firefoxProfile = new FirefoxProfile();
+        desiredCapabilities
+            .setCapability(FirefoxDriver.PROFILE, firefoxProfile);
+        return desiredCapabilities;
+    }
+
+    @Override public DesiredCapabilities create(Browser browser,
+        String version) {
         return create(browser);
     }
 }
