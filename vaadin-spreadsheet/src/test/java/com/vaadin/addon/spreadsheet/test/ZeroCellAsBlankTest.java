dde48b5a8ffa76575404111b5e0db1145ca37995
mtzukanov <mtzukanov@vaadin.com>
2017-02-28 18:06:05 +0100

    task #2: Show zero values as blank feature support

---
+++ vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/ZeroCellAsBlankTest.java
@@ -0,0 +1,38 @@
+package com.vaadin.addon.spreadsheet.test;
+
+import com.vaadin.addon.spreadsheet.elements.SpreadsheetElement;
+import com.vaadin.addon.spreadsheet.test.pageobjects.SpreadsheetPage;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ZeroCellAsBlankTest extends AbstractSpreadsheetTestCase {
+
+    @Test
+    public void zeroCellAsBlank_loadSheetWithSettingOn_zeroExpected() throws Exception {
+        SpreadsheetPage spreadsheetPage = headerPage.loadFile(
+                "show-zero-cells.xlsx", this);
+
+        SpreadsheetElement spreadsheet = $(SpreadsheetElement.class).first();
+        spreadsheet.selectSheetAt(0);
+
+        assertEquals("2", spreadsheetPage.getCellValue("B2"));
+        assertEquals("0", spreadsheetPage.getCellValue("B3"));
+        assertEquals("0", spreadsheetPage.getCellValue("B4"));
+        assertEquals("0", spreadsheetPage.getCellValue("B5"));
+    }
+
+    @Test
+    public void zeroCellAsBlank_loadSheetWithSettingOn_emptyExpected() throws Exception {
+        SpreadsheetPage spreadsheetPage = headerPage.loadFile(
+                "show-zero-cells.xlsx", this);
+
+        SpreadsheetElement spreadsheet = $(SpreadsheetElement.class).first();
+        spreadsheet.selectSheetAt(1);
+
+        assertEquals("2", spreadsheetPage.getCellValue("B2"));
+        assertEquals("", spreadsheetPage.getCellValue("B3"));
+        assertEquals("0", spreadsheetPage.getCellValue("B4"));
+        assertEquals("", spreadsheetPage.getCellValue("B5"));
+    }
+}
