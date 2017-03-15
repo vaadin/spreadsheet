2e0277e5cbb83c54fbe2487a7d2b75f4fe87c2ab
Chiara Marcheschi <chiaramarcheschi@tagetik.com>
2017-03-15 17:00:17 +0100

    task #32 fixed regression after merging

--- vaadin-spreadsheet/src/main/java/com/vaadin/addon/spreadsheet/ConditionalFormatter.java
+++ vaadin-spreadsheet/src/main/java/com/vaadin/addon/spreadsheet/ConditionalFormatter.java
@@ -777,7 +777,7 @@ public class ConditionalFormatter implements Serializable {
             // not sure if this is used, since no boolean option exists in
             // Excel..
 
-            Boolean formulaVal = Boolean.parseBoolean(rule.getFormula1());
+            boolean formulaVal = ((BoolEval)eval).getBooleanValue();
 
             switch (comparisonOperation) {
             case ComparisonOperator.EQUAL:
