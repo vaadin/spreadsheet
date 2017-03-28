8b0db0474024d0eec792274c53c6df3dacbe29b5
Chiara Marcheschi <chiaramarcheschi@tagetik.com>
2017-03-28 10:59:11 +0200

    Evaluation of conditional formatting if cell is empty - Added test of CF based on numeric formula in a empty cell (#32)

--- vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/ConditionalFormattingBasedOnFormulaTest.java
+++ vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/ConditionalFormattingBasedOnFormulaTest.java
@@ -11,6 +11,7 @@ public class ConditionalFormattingBasedOnFormulaTest
     private static final String VALUE = "'Fooooooooooooooooooooooooo";
     private static final String FALSE_CONDITION_COLOR = "rgba(255, 255, 255, 1)";
     private static final String TRUE_CONDITION_COLOR = "rgba(255, 0, 0, 1)";
+    private static final String DIFFERENT_FROM_ZERO_CONDITION_COLOR = "rgba(255, 255, 0, 1)";
     
     private SpreadsheetPage spreadsheetPage;
 
@@ -34,5 +35,17 @@ public class ConditionalFormattingBasedOnFormulaTest
         spreadsheetPage.setCellValue("A1", VALUE);
         spreadsheetPage.setCellValue("A2", VALUE);
         assertEquals(TRUE_CONDITION_COLOR, spreadsheetPage.getCellColor("A2"));
+    }
+    
+    @Test
+    public void loadSpreadsheetWithConditionalFormattingRulesInRow10_EvaluateFormatting_CheckColorOfCells(){
+        String a10WithConditionEqualsToOne = spreadsheetPage.getCellColor("A10");
+        String b10WithConditionEqualsToZero = spreadsheetPage.getCellColor("B10");
+        String c10WithConditionGreaterOfZero = spreadsheetPage.getCellColor("C10");
+        String d10WithConditionLowerOfZero = spreadsheetPage.getCellColor("D10");
+        assertEquals(DIFFERENT_FROM_ZERO_CONDITION_COLOR, a10WithConditionEqualsToOne);
+        assertEquals(FALSE_CONDITION_COLOR, b10WithConditionEqualsToZero);
+        assertEquals(DIFFERENT_FROM_ZERO_CONDITION_COLOR, c10WithConditionGreaterOfZero);
+        assertEquals(DIFFERENT_FROM_ZERO_CONDITION_COLOR, d10WithConditionLowerOfZero);
     }
 }
