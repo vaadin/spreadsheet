98b046f54f5a634a3783e4593f0b4c47c7311ce7
mtzukanov <mtzukanov@vaadin.com>
2017-05-12 09:04:04 +0300

    #5 Tests for entering values and changing locale

--- vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/CustomFormatTest.java
+++ vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/CustomFormatTest.java
@@ -7,10 +7,7 @@ import org.junit.Test;
 
 import com.vaadin.addon.spreadsheet.elements.SheetCellElement;
 import com.vaadin.addon.spreadsheet.elements.SpreadsheetElement;
-import com.vaadin.testbench.annotations.RunLocally;
-import com.vaadin.testbench.parallel.Browser;
 
-@RunLocally(Browser.PHANTOMJS)
 public class CustomFormatTest extends AbstractSpreadsheetTestCase {
 
     private SpreadsheetElement spreadSheet;
@@ -22,12 +19,12 @@ public class CustomFormatTest extends AbstractSpreadsheetTestCase {
         headerPage.loadFile("custom_format.xlsx", this);
 
         spreadSheet = $(SpreadsheetElement.class).first();
+
+        setLocale(Locale.US);
     }
 
     @Test
     public void assertCorrectValuesAfterLoading() {
-        setLocale(Locale.US);
-
         assertNumbersColumn();
         assertLiteralsColumn();
         assertEmptyColumn();
@@ -115,146 +112,43 @@ public class CustomFormatTest extends AbstractSpreadsheetTestCase {
         assertCellValue("G19", "text");
     }
 
-//    @Test
-//    public void customFormatCellWithFourParts_enterZero_getsFormatted()
-//        throws Exception {
-//        // changes into minus, Excel also adds space on both sides and fills the
-//        // left side with whitespace
-//        assertCellFormat("C3", "0", "-", Locale.US);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithFourParts_enterText_noChange()
-//        throws Exception {
-//        //adds a double quote on text values
-//        assertCellFormat("C4", "text", "\"text\"", Locale.US);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithFourParts_enterNumberWithITlocale_getsFormatted()
-//        throws Exception {
-//        //number formatted in European format
-//        assertCellFormat("C1", "=5555555.5", "5.555.555,5", Locale.ITALY);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithThreeParts_enterPositiveNumber_becomesNumberType()
-//        throws Exception {
-//        // numbers are right-aligned
-//        assertAlignment("right", "C6");
-//        assertCellFormat("C6", "5", "5.0", Locale.US);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithThreeParts_enterNegativeNumber_getsFormatted()
-//        throws Exception {
-//        // drops minus and gains parentheses
-//        assertCellFormat("C6", "-5", "(5.0)", Locale.US);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithThreeParts_enterZero_getsFormatted()
-//        throws Exception {
-//
-//        // changes into minus, Excel also adds space on both sides and fills the
-//        // left side with whitespace
-//        assertCellFormat("C6", "0", "-", Locale.US);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithThreeParts_enterText_noChange()
-//        throws Exception {
-//
-//        assertCellFormat("C6", "text", "text", Locale.US);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithThreeParts_enterNumberWithITlocale_getsFormatted()
-//        throws Exception {
-//        assertCellFormat("C6", "=5555555.5", "5.555.555,5", Locale.ITALY);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithTwoParts_enterPositiveNumber_becomesNumberType()
-//        throws Exception {
-//        // numbers are right-aligned
-//        assertAlignment("right", "C11");
-//        assertCellFormat("C11", "5", "5.0", Locale.US);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithTwoParts_enterNegativeNumber_getsFormatted()
-//        throws Exception {
-//        // drops minus and gains parentheses
-//        assertCellFormat("C11", "-5", "(5.0)", Locale.US);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithTwoParts_enterZero_getsFormatted()
-//        throws Exception {
-//        assertCellFormat("C11", "0", "0.0", Locale.US);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithTwoParts_enterText_noChange()
-//        throws Exception {
-//
-//        assertCellFormat("C11", "text", "text", Locale.US);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithTwoParts_enterNumberWithITlocale_getsFormatted()
-//        throws Exception {
-//        assertCellFormat("C11", "=5555555.5", "5.555.555,5", Locale.ITALY);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithOnePart_enterPositiveNumber_becomesNumberType()
-//        throws Exception {
-//        // numbers are right-aligned
-//        assertAlignment("right", "C11");
-//        assertCellFormat("C16", "5", "5.0", Locale.US);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithOnePart_enterNegativeNumber_getsFormatted()
-//        throws Exception {
-//        assertCellFormat("C16", "-5", "-5.0", Locale.US);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithOnePart_enterZero_getsFormatted()
-//        throws Exception {
-//        assertCellFormat("C16", "0", "0.0", Locale.US);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithOnePart_enterText_noChange()
-//        throws Exception {
-//
-//        assertCellFormat("C16", "text", "text", Locale.US);
-//    }
-//
-//    @Test
-//    public void customFormatCellWithOnePart_enterNumberWithITlocale_getsFormatted()
-//        throws Exception {
-//        assertCellFormat("C16", "=5555555.5", "5.555.555,5", Locale.ITALY);
-//    }
-//
-//    private void assertCellFormat(String cellID, String value, String expected,
-//        Locale locale) {
-//        SheetCellElement formatCell = spreadSheet.getCellAt(cellID);
-//        formatCell.setValue(value);
-//        setLocale(locale);
-//        assertEquals(expected, formatCell.getValue());
-//    }
+    @Test
+    public void customFormatFourParts_enterNumberWith_getsFormatted()
+        throws Exception {
+        assertCellFormatAfterEntry("C1", "=5555555.5", "5.555.555,5",
+            Locale.ITALY);
+    }
+
+    @Test
+    public void customFormatThreeParts_enterNumber_getsFormatted()
+        throws Exception {
+        assertCellFormatAfterEntry("C6", "=5555555.5", "5.555.555,5",
+            Locale.ITALY);
+    }
+
+    @Test
+    public void customFormatTwoParts_enterNumber_getsFormatted()
+        throws Exception {
+        assertCellFormatAfterEntry("C11", "=5555555.5", "5.555.555,5",
+            Locale.ITALY);
+    }
+
+    @Test
+    public void customFormatOnePart_enterNumber_getsFormatted()
+        throws Exception {
+        assertCellFormatAfterEntry("C16", "=5555555.5", "5.555.555,5",
+            Locale.ITALY);
+    }
+
+    private void assertCellFormatAfterEntry(String cellID, String value,
+        String expected, Locale locale) {
+        SheetCellElement formatCell = spreadSheet.getCellAt(cellID);
+        formatCell.setValue(value);
+        setLocale(locale);
+        assertEquals(expected, formatCell.getValue());
+    }
 
     private void assertCellValue(String cell, String value) {
         assertEquals(value, spreadSheet.getCellAt(cell).getValue());
-    }
-
-    private void assertAlignment(String expectedAlignment, String cellId) {
-        SheetCellElement formatCell = spreadSheet.getCellAt(cellId);
-        assertEquals(expectedAlignment, formatCell.getCssValue("text-align"));
     }
 }
