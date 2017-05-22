21d8156f9a59baf726883033dc0b2acfe006f01e
mtzukanov <mtzukanov@vaadin.com>
2017-05-22 16:56:19 +0300

    #42 Tests corrected and extended

--- vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/ConditionalFormattingCellValueIsTest.java
+++ vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/ConditionalFormattingCellValueIsTest.java
@@ -3,18 +3,23 @@ package com.vaadin.addon.spreadsheet.test;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import com.vaadin.addon.spreadsheet.test.pageobjects.SpreadsheetPage;
 
-public class ConditionalFormattingCellValueIsTest extends AbstractSpreadsheetTestCase {
+import com.vaadin.addon.spreadsheet.test.pageobjects.SpreadsheetPage;
+import com.vaadin.testbench.annotations.RunLocally;
+import com.vaadin.testbench.parallel.Browser;
+
+@RunLocally(Browser.PHANTOMJS)
+public class ConditionalFormattingCellValueIsTest
+    extends AbstractSpreadsheetTestCase {
 
     private static final String STRING_VALUE = "'Foo";
     private static final String NUMBER_VALUE = "1";
     private static final String DIFFERENT_NUMBER_VALUE = "2";
-    private static final String TRUE_VALUE = "TRUE";
-    private static final String FALSE_VALUE = "FALSE";
+    private static final String TRUE_VALUE = "=1<2";
+    private static final String FALSE_VALUE = "=1>2";
     private static final String FALSE_CONDITION_COLOR = "rgba(255, 255, 255, 1)";
     private static final String TRUE_CONDITION_COLOR = "rgba(255, 0, 0, 1)";
-    
+
     private SpreadsheetPage spreadsheetPage;
 
     @Override
@@ -26,9 +31,9 @@ public class ConditionalFormattingCellValueIsTest extends AbstractSpreadsheetTes
     }
 
     @Test
-    public void loadSpreadsheetWithEqualConditionFormattingInB3_MakeConditionFalse_CellB3FilledWhite() {
+    public void B3D3F3RedIfNotEqualToCellAbove_makeConditionsFalse_filledWhite() {
         spreadsheetPage.setCellValue("B2", STRING_VALUE);
-        spreadsheetPage.setCellValue("B3", "Not"+STRING_VALUE);
+        spreadsheetPage.setCellValue("B3", "Not" + STRING_VALUE);
 
         spreadsheetPage.setCellValue("D2", NUMBER_VALUE);
         spreadsheetPage.setCellValue("D3", DIFFERENT_NUMBER_VALUE);
@@ -46,7 +51,7 @@ public class ConditionalFormattingCellValueIsTest extends AbstractSpreadsheetTes
     }
 
     @Test
-    public void loadSpreadsheetWithEqualConditionFormattingInB3_MakeConditionTrue_CellB3FilledRed() {
+    public void B3D3F3RedIfNotEqualToCellAbove_makeConditionsTrue_filledRed() {
         spreadsheetPage.setCellValue("B2", STRING_VALUE);
         spreadsheetPage.setCellValue("B3", STRING_VALUE);
 
@@ -55,7 +60,6 @@ public class ConditionalFormattingCellValueIsTest extends AbstractSpreadsheetTes
 
         spreadsheetPage.setCellValue("F2", TRUE_VALUE);
         spreadsheetPage.setCellValue("F3", TRUE_VALUE);
-
 
         String cellColorStringCase = spreadsheetPage.getCellColor("B3");
         String cellColorNumberCase = spreadsheetPage.getCellColor("D3");
@@ -67,30 +71,60 @@ public class ConditionalFormattingCellValueIsTest extends AbstractSpreadsheetTes
     }
 
     @Test
-    public void loadSpreadsheetWithNotEqualConditionFormattingInB4_insertIncoherentValue_CellB4FilledRed() {
-        spreadsheetPage.setCellValue("B2", STRING_VALUE);
-        spreadsheetPage.setCellValue("B4", NUMBER_VALUE);
+    public void B4RedIfNotEqualsB3_insertIncoherentValue_B4FilledRed() {
+        spreadsheetPage.setCellValue("B3", "1");
+        spreadsheetPage.setCellValue("B4", "1");
+        assertEquals(FALSE_CONDITION_COLOR, spreadsheetPage.getCellColor("B4"));
+
+        spreadsheetPage.setCellValue("B3", "1");
+        spreadsheetPage.setCellValue("B4", "'1");
         assertEquals(TRUE_CONDITION_COLOR, spreadsheetPage.getCellColor("B4"));
     }
 
     @Test
-    public void loadSpreadsheetWithNotEqualToNumberZeroConditionFormattingRuleInBlankCell_insertValue_CellB5FilledRed() {
+    public void B5RedIfNotEqualsToZero_insertDifferentValues_B5FilledCorrectly() {
+        spreadsheetPage.setCellValue("B5", "0");
         assertEquals(FALSE_CONDITION_COLOR, spreadsheetPage.getCellColor("B5"));
-        spreadsheetPage.setCellValue("B5", NUMBER_VALUE);
+
+        spreadsheetPage.setCellValue("B5", "1");
         assertEquals(TRUE_CONDITION_COLOR, spreadsheetPage.getCellColor("B5"));
+
+        spreadsheetPage.deleteCellValue("B5");
+        assertEquals(FALSE_CONDITION_COLOR, spreadsheetPage.getCellColor("B5"));
     }
 
     @Test
-    public void loadSpreadsheetWithEqualToNumberZeroConditionFormattingRuleInBlankCell_insertValue_CellB6FilledWhite() {
+    public void B6RedIfEqualsToZero_insertValues_B6FilledCorrectly() {
+        spreadsheetPage.setCellValue("B6", "0");
         assertEquals(TRUE_CONDITION_COLOR, spreadsheetPage.getCellColor("B6"));
-        spreadsheetPage.setCellValue("B6", NUMBER_VALUE);
+
+        spreadsheetPage.setCellValue("B6", "1");
         assertEquals(FALSE_CONDITION_COLOR, spreadsheetPage.getCellColor("B6"));
+
+        spreadsheetPage.deleteCellValue("B6");
+        assertEquals(TRUE_CONDITION_COLOR, spreadsheetPage.getCellColor("B6"));
     }
 
     @Test
-    public void loadSpreadsheetWithNotEqualToBooleanFalseConditionFormattingRuleInBlankCell_insertValue_CellB7FilledRed() {
-        assertEquals(FALSE_CONDITION_COLOR, spreadsheetPage.getCellColor("B7"));
+    public void B7RedIfNotEqualsToBooleanFalse_insertValues_B7FilledRed() {
         spreadsheetPage.setCellValue("B7", FALSE_VALUE);
+        assertEquals(FALSE_CONDITION_COLOR, spreadsheetPage.getCellColor("B7"));
+
+        spreadsheetPage.setCellValue("B7", TRUE_VALUE);
         assertEquals(TRUE_CONDITION_COLOR, spreadsheetPage.getCellColor("B7"));
+
+        spreadsheetPage.deleteCellValue("B7");
+        assertEquals(FALSE_CONDITION_COLOR, spreadsheetPage.getCellColor("B7"));
+    }
+
+    @Test
+    public void B2RedIfEqualsB3_insertValues_B3FilledCorrectly() {
+        spreadsheetPage.setCellValue("B2", "1");
+        spreadsheetPage.setCellValue("B3", "2");
+        assertEquals(FALSE_CONDITION_COLOR, spreadsheetPage.getCellColor("B3"));
+
+        spreadsheetPage.deleteCellValue("B2");
+        spreadsheetPage.deleteCellValue("B3");
+        assertEquals(TRUE_CONDITION_COLOR, spreadsheetPage.getCellColor("B3"));
     }
 }
