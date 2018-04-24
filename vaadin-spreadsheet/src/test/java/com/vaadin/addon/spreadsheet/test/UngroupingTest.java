678f0c773b6189c59567a431192149b632392d43
Guillermo Alvarez <guillermo@vaadin.com>
2018-04-24 11:23:47 +0300

    Add alternative references and make tests more reliable

--- vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/UngroupingTest.java
+++ vaadin-spreadsheet/src/test/java/com/vaadin/addon/spreadsheet/test/UngroupingTest.java
@@ -1,23 +1,24 @@
 package com.vaadin.addon.spreadsheet.test;
 
-import com.vaadin.addon.spreadsheet.test.pageobjects.SpreadsheetPage;
-import org.junit.Test;
-import org.openqa.selenium.WebElement;
+import static org.junit.Assert.assertFalse;
 
 import java.util.List;
 
-import static org.junit.Assert.assertFalse;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.addon.spreadsheet.test.pageobjects.SpreadsheetPage;
 
 public class UngroupingTest extends AbstractSpreadsheetTestCase {
 
-    private SpreadsheetPage spreadsheetPage;
 
     /**
      * Ticket 599#
      */
     @Test
     public void grouping_expandColumnGroup_groupingElementsHaveCorrectValues() throws Exception {
-        spreadsheetPage = headerPage.loadFile("ungrouping_cellUpdating.xlsx", this);
+        loadPage("ungrouping_cellUpdating.xlsx");
+        SpreadsheetPage spreadsheetPage = new SpreadsheetPage(driver);
         List<WebElement> groupings = spreadsheetPage.getGroupings();
         groupings.get(1).click();
         String cellValue = spreadsheetPage.getCellValue(3, 1);
