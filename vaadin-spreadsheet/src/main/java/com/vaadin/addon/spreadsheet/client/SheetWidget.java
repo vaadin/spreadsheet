7a91487aba5661ef3959af88dbf3b961a3f58b2f
Alex <alex@vaadin.com>
2018-11-12 15:56:32 +0200

    Fix vertical overscroll
    There is extra 1 px scroll per cell, which accumulates a big overscroll

--- vaadin-spreadsheet/src/main/java/com/vaadin/addon/spreadsheet/client/SheetWidget.java
+++ vaadin-spreadsheet/src/main/java/com/vaadin/addon/spreadsheet/client/SheetWidget.java
@@ -5440,8 +5440,6 @@ public class SheetWidget extends Panel {
             for (int i = topRowIndex - 1; i >= row - 1 && i > 0; i--) {
                 scroll += getRowHeight(i);
             }
-            // with horizontal need to add 1 pixel per cell because borders
-            scroll += (topRowIndex - row);
             final int result = sheet.getScrollTop() - scroll;
             sheet.setScrollTop(result > 0 ? result : 0);
             if (row <= firstRowIndex
@@ -5458,8 +5456,6 @@ public class SheetWidget extends Panel {
                         && i <= maximumRows; i++) {
                     scroll += getRowHeight(i);
                 }
-                // with horizontal need to add 1 pixel per cell because borders
-                scroll += (row - bottomRowIndex);
                 sheet.setScrollTop(sheet.getScrollTop() + scroll);
                 if (row >= lastRowIndex
                         || scroll > (actionHandler.getRowBufferSize() / 2)) {
@@ -5561,8 +5557,6 @@ public class SheetWidget extends Panel {
                 for (int i = topRowIndex - 1; i >= row1 - 1 && i > 0; i--) {
                     scroll += getRowHeight(i);
                 }
-                // with horizontal need to add 1 pixel per cell because borders
-                scroll += (topRowIndex - row1);
                 final int result = sheet.getScrollTop() - scroll;
                 sheet.setScrollTop(result > 0 ? result : 0);
                 if (row1 <= firstRowIndex
@@ -5577,8 +5571,6 @@ public class SheetWidget extends Panel {
                         && i <= maximumRows; i++) {
                     scroll += getRowHeight(i);
                 }
-                // with horizontal need to add 1 pixel per cell because borders
-                scroll += (row1 - bottomRowIndex);
                 sheet.setScrollTop(sheet.getScrollTop() + scroll);
                 if (row1 >= lastRowIndex
                         || scroll > (actionHandler.getRowBufferSize() / 2)) {
