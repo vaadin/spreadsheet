c34040dad56b7e51339811d1c2b31ae7c03357ab
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2022-03-28 13:38:56 +0300

    --wip-- [skip ci]

---
+++ vaadin-spreadsheet-flow-integration-tests/src/test/java/com/vaadin/flow/component/spreadsheet/tests/SpreadSheetIT.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2000-2022 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.spreadsheet.tests;
+
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.testbench.TestBenchTestCase;
+import com.vaadin.flow.testutil.AbstractComponentIT;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Integration tests for the ButtonView.
+ */
+@TestPath("vaadin-spreadsheet")
+public class SpreadSheetIT extends AbstractComponentIT {
+
+    private TestBenchTestCase layout;
+
+    @Before
+    public void init() {
+        open();
+        layout = this;
+        waitForElementPresent(By.tagName("vaadin-spreadsheet"));
+    }
+
+    @Test
+    public void clickOnDefaultButton_textIsDisplayed() {
+        WebElement button = layout.findElement(By.id("vaadin-spreadsheet"));
+    }
+
+    @Override
+    protected String getTestPath() {
+        return ("/vaadin-button");
+    }
+}
